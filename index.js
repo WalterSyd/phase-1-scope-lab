98dc0135f9f264dfdcebc2c11db65f1227967ac1
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-07-20 16:13:42 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+// Declare customerName variable in global scope
+var customerName = "bob";
+
+//Function to upperCaseCustomerName
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+//Function to set Best Customer
+function setBestCustomer(){
+    bestCustomer = "not bob";  //overwrites bestCustomer();
+}
+
+//Function to overwrite Best customer
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someone";
+//Function change leastFavoriteCustomer
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "someone else";
+}
